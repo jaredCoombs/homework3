e33942ca9361f26f003f251e8bea312db2e8b000
Jared Coombs <jared.coombs@gmail.com>
2020-02-07 15:55:33 -0800

    added charachter variables

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 //array of all charachters to be included in password. Upper, lower case. Numbers and special charachters.
+var numberCharachters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+var lowerCaseLetters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x,', 'y', 'z'];
+var upprCaseLetters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+var specialCharachters = ['~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '+',
+  '[', '{', ']', '}',];
 
 
 //Need prompts for password options after users after button pushed
