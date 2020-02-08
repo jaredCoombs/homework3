83bd7e630fda0286ddfc22cbb14755a91bb10e3f
Jared Coombs <jared.coombs@gmail.com>
2020-02-07 21:03:50 -0800

    user inputs completed

--- script.js
+++ script.js
@@ -7,64 +7,69 @@ var specialCharachters = ['~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')',
 
 
 //Need prompts for password options after users after button pushed
-var length = parseInt(prompt("How many characters do you want for your new password?"));
+var length = parseInt(prompt("How many characters do you want for your new password?")
+);
 if (isNaN(length) === true) {
   alert("Oops! Password length must be stated as a number.");
-  if (length < 8) {
-    alert("Uh oh! Password must have at least 8 charachters.");
 
-  }
+}
+if (length < 8) {
+  alert("Uh oh! Password must have at least 8 charachters.");
 
-  if (length > 128) {
-    alert("Doh! Password must have less than 129 charachters");
+}
 
-  }
+if (length > 128) {
+  alert("Doh! Password must have less than 129 charachters");
 
-  var hasSpecialCharachters = confirm(
-    "Click OK to confirm using special charachters."
-  );
+}
 
-  var hasNumericCharacters = confirm(
-    "Click OK to confirm using numeric charachters."
-  );
+var hasSpecialCharachters = confirm(
+  "Click OK to confirm using special charachters."
+);
 
-  var hasUpperCaseCharachters = confirm(
-    "Click OK to confirm using upper case charachters."
-  );
+var hasNumericCharacters = confirm(
+  "Click OK to confirm using numeric charachters."
+);
 
-  var hasLowerCaseCharachters = confirm(
-    "Click to confirm using lower case charachters."
-  );
+var hasUpperCaseCharachters = confirm(
+  "Click OK to confirm using upper case charachters."
+);
 
-  if (
-    hasSpecialCharachters === false &&
-    hasNumericCharachters === false &&
-    hasUpperCaseCharachters === false &&
-    hasLowerCAseCharachters === false
-  ) {
-    alert("Please select at least one charachter type");
+var hasLowerCaseCharachters = confirm(
+  "Click to confirm using lower case charachters."
+);
 
-  }
+if (
+  hasSpecialCharachters === false &&
+  hasNumericCharachters === false &&
+  hasUpperCaseCharachters === false &&
+  hasLowerCAseCharachters === false
+) {
+  alert("Please select at least one charachter type");
 
-
-
-  //Need a way to store user input
-
-  //Need a function to use at least one of  each of the charachter types
+}
 
 
 
-  // Assignment Code
-  var generateBtn = document.querySelector("#generate");
+//Need a way to store user input
 
-  // Write password to the #password input
-  function writePassword() {
-    var password = generatePassword();
-    var passwordText = document.querySelector("#password");
+//Need a function to use at least one of  each of the charachter types
 
-    passwordText.value = password;
 
-  }
 
-  // Add event listener to generate button
-  generateBtn.addEventListener("click", writePassword);
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+
